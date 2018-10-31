b7c02889259267c288e02f7a3c1d38f12cc4ecf6
chan328 <rkdcks020328@gmail.com>
2018-10-31 11:30:56 +0900

    10/31

---
+++ for.py
@@ -0,0 +1,7 @@
+family = ['mother', 'father', 'brother', 'son']
+for x in family:
+	print('%s %s' % (x, len(x)))
+
+a = [4, 5, 6, 7]
+for i in a:
+        print(i)
---
+++ if.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+if a > b:
+	print(a)
+else:
+	print(b)
+
+c = 3
+d = 4
+if c > d:
+        print(c)
+elif d > c:
+        print(d)                
